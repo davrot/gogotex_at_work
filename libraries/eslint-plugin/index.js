c725e91e71cc93db546ea21a681ac37a8684161d
David Rotermund <davrot@neuro.uni-bremen.de>
2025-12-12 23:58:38 +0000

    chore: provide minimal @overleaf/eslint-plugin rules as stubs for local lint

--- libraries/eslint-plugin/index.js
+++ libraries/eslint-plugin/index.js
@@ -1,4 +1,19 @@
 // Minimal stub for @overleaf/eslint-plugin used in monorepo
+function noopRule() {
+  return {
+    meta: { type: 'suggestion', docs: { description: 'noop rule' } },
+    create: () => ({}),
+  }
+}
+
 module.exports = {
-  rules: {},
+  rules: {
+    'prefer-kebab-url': noopRule(),
+    'require-vi-doMock-valid-path': noopRule(),
+    'require-script-runner': noopRule(),
+    'no-generated-editor-themes': noopRule(),
+    'no-unnecessary-trans': noopRule(),
+    'should-unescape-trans': noopRule(),
+    'require-loading-label': noopRule(),
+  },
 };
